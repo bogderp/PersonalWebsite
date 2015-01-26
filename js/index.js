0bbdcd53ff01232c92682b33e7cc7c848349bcb2
bogdanpozderca <bogdanpozderca@gmail.com>
2015-01-25 19:20:03 -0500

    Mobile Support, Aesthetic Changes, Linkedin Button
    Mobile Support via Mobile Menu Button
    Linkedin Button next to Github button.
    Fonts bigger for mobile.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
-function dynamicHeight() {
+function dynamics() {
     var height = $(window).height();
-
+    var width = $(window).width();
     height = parseInt(height) + 'px';
+    width = parseInt(width) + 'px';
     $(".mainView").css('height',height);
+    $(".mobileMenu ul").css('width', width);
 }
 
 $(document).ready(function(){
     $(window).on('beforeunload', function() {
         $(window).scrollTop(0);
     });
-    dynamicHeight();
+    dynamics();
+
+    isVisible = 0;
+    $('.mobileMenu').click(function(){
+    	if(isVisible) {
+    		$('.mobileMenu li').fadeOut('slow');
+    		isVisible = 0;
+    	} else {
+    		$('.mobileMenu li').fadeIn('slow');
+    		isVisible = 1;
+    	}
+    	
+    })
 });
