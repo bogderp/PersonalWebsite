62028a45e917938f8453a39cbd5c1074e7e0eebb
Bogdan Pozderca <bogdanpozderca@gmail.com>
2015-10-07 16:46:53 -0400

    when scroling to projects, border of navbar changes now

--- js/index.js
+++ js/index.js
@@ -11,3 +11,22 @@ $(document).ready(function(){
     });
 
 });
+
+var desktop_menu = [
+  {"scroll_to": "#home-nav", "elem": $("#hero")},
+  {"scroll_to": "#faq-nav", "elem": $("#faq")},
+  {"scroll_to": "#mobile-nav", "elem": $("#mobile")},
+  {"scroll_to": "#contact-nav", "elem": $("#contact")},
+];
+
+$(window).scroll(function() {
+    var halfHeight = $(this).scrollTop() ;
+    var topOffset = $("#projects").offset().top;
+    var height = $("#projects").height();
+
+    if(halfHeight >= topOffset && halfHeight < (topOffset + height)) {
+      $(".navbar").css("border-color", "#BDD4DE");
+    } else {
+      $(".navbar").css("border-color", "transparent");
+    }
+});
