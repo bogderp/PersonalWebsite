eb616d718b37a143625650850da680b466b912aa
Bogdan Pozderca <bogdanpozderca@gmail.com>
2017-11-13 15:08:05 -0500

    iPhone X Compatibility

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1 viewport-fit=cover">
     <meta name="description" content="">
     <meta name="author" content="Bogdan Pozderca">
     <!-- <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"> -->
